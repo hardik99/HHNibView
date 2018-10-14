edd31f0f7ce4a450e444e900ff2d122d2754c297
hardik99 <hardik.halani14@gmail.com>
2018-10-14 23:46:10 +0530

    initial commit

---
+++ Example/HHNibView/CustomView.swift
@@ -0,0 +1,15 @@
+//
+//  CustomView.swift
+//  HHView_Example
+//
+//  Created by Hardik on 14/10/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import HHNibView
+
+class CustomView: HHNibView {
+
+
+}
---
+++ Example/Pods/Target Support Files/HHNibView/HHNibView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HHNibView : NSObject
+@end
+@implementation PodsDummy_HHNibView
+@end
