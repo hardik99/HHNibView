8c3edaf36d85e21ae3e7da077e4b44661b2a33a9
hardik99 <hardik.halani14@gmail.com>
2018-10-14 23:50:58 +0530

    initial commit

--- HHNibView.podspec
+++ HHNibView.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'HHNibView'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of HHNibView.'
+  s.summary          = 'UIView super class for managing xib easily.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+TODO: Create a new xib using UIView. Change super class of your custom view to HHNibView. Add your custom class as file owner in xib. Please note that your custom class and xib must have same name.
                        DESC
 
   s.homepage         = 'https://github.com/hardik99/HHNibView'
---
+++ HHNibView/Classes/HHNibView.swift
@@ -0,0 +1,52 @@
+//
+//  HHView.swift
+//
+//  Created by Hardik on 14/10/18.
+//  Copyright © 2018 Hardik. All rights reserved.
+//
+
+import UIKit
+
+open class HHNibView: UIView {
+
+    var view: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupView()
+    }
+
+    private func setupView(){
+        backgroundColor = .clear
+        self.view = loadNib()
+        self.view.frame = self.bounds
+        self.addSubview(self.view)
+        
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[childView]|",
+                                                      options: [],
+                                                      metrics: nil,
+                                                      views: ["childView": view]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[childView]|",
+                                                      options: [],
+                                                      metrics: nil,
+                                                      views: ["childView": view]))
+    }
+}
+
+extension UIView {
+    /** Loads instance from nib with the same name. */
+    func loadNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
+}
